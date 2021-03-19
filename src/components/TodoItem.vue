5e685c43ddaf4adb9dec604263bdb84c6efac294
mei.wu <mei.wu@eyesmedia.com.tw>
2021-03-19 14:46:07 +0800

    todoItem add

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <li>
+    <template v-if="!edit">
+      <input type="checkbox" v-model="editingTodo.done" />{{ tId }}:
+      {{ todo.content }}
+    </template>
+    <template v-else>
+      <input type="text" v-model="editingTodo.content" />
+    </template>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    },
+    tId: Number,
+    edit: {
+      type: Boolean,
+      defalut: false
+    }
+  },
+  data () {
+    return {
+      editingTodo: {
+        content: '',
+        done: false
+      }
+    }
+  },
+  mounted () {
+    this.editingTodo.content = this.todo.content
+    this.editingTodo.done = this.todo.done
+  }
+}
+</script>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,24 +1,50 @@
 <template>
   <div class="todo">
     <h1>This is an todo page</h1>
-     <!-- replace替代自己的歷史記錄 -->
-    <router-link to="/todo" :class="{'active': defActive}" replace>all</router-link>｜
-    <router-link to="/todo?filter=active" replace>active</router-link>｜
-    <router-link :to="{ path:'/todo', query:{ filter:'done' }}" replace>do</router-link>
+    <!-- replace替代自己的歷史記錄 -->
+    <router-link to="/todo" :class="{ active: defActive }" replace
+      >all</router-link
+    >｜ <router-link to="/todo?filter=active" replace>active</router-link>｜
+    <router-link :to="{ path: '/todo', query: { filter: 'done' } }" replace
+      >do</router-link
+    >
     <!-- <router-link :to="{ name:'Todo', query:{ filter:'done' }}">do</router-link>｜ -->
-    <p>show: {{ filter }}</p>
-    <p>{{ list }}</p>
+    <!-- <p>show: {{ filter }}</p>
+    <p>{{ list }}</p> -->
+    <ul>
+      <TodoItem v-for="item of list" :key="item.tId+''+item.todo.content"
+      :todo="item.todo" :tId="item.tId" :edit="item.tId === edit" />
+
+      <!-- <li v-for="item of list" :key="item.tId">
+        <template v-if="edit !== item.tId">
+          <input type="checkbox" v-model="item.todo.done" />{{ item.tId }}:
+          {{ item.todo.content }}
+        </template>
+        <template v-else>
+          <input type="text" v-model="item.todo.content" />
+        </template>
+      </li> -->
+
+      <!-- <li v-for="item of list" :key="item.tId">
+        <input type="checkbox" v-model="item.todo.done">{{item.tId}}: {{item.todo.content}}
+      </li> -->
+    </ul>
   </div>
 </template>
 
 <script>
+import TodoItem from '@/components/TodoItem'
 export default {
   data () {
     return {
       // all, active, done
       filter: 'all',
-      defActive: false
+      defActive: false,
+      edit: null
     }
+  },
+  components: {
+    TodoItem
   },
   computed: {
     list () {
@@ -39,7 +65,9 @@ export default {
       // immediate進入馬上執行
       immediate: true,
       handler: function (route) {
-        (route.query.filter === undefined) ? this.defActive = true : this.defActive = false
+        route.query.filter === undefined
+          ? (this.defActive = true)
+          : (this.defActive = false)
         this.filter = route.query.filter || 'all'
       }
     }
